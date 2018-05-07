5110fe14517b519c356690569577e0514b3f6a80
Samuel Carlsson <samuel.carlsson@volumental.com>
2018-05-07 16:18:47 +1200

    create .whl file

---
+++ setup.py
@@ -0,0 +1,37 @@
+import os
+from setuptools import find_packages, setup
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+
+with open('README.md') as f:
+    README = f.read()
+
+
+setup(
+    name='django-leek',
+    version='0.1',
+    packages=find_packages(),
+    include_package_data=True,
+    license='MIT License',
+    description='A simple Django app to offload tasks from main web server',
+    long_description=README,
+    long_description_content_type='text/markdown',
+    url='https://github.com/Volumental/django-leek',
+    author='Samuel Carlsson',
+    author_email='samuel.carlsson@volumental.com',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Framework :: Django :: 1.9',  # replace "X.Y" as appropriate
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',  # example license
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+)
