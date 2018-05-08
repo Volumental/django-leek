d50d653db8b94ddc0ab8ec666fda65076c600777
Samuel Carlsson <samuel.carlsson@volumental.com>
2018-05-08 15:28:33 +1200

    v0.2

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open('README.md') as f:
 
 setup(
     name='django-leek',
-    version='0.1',
+    version='0.2',
     packages=['django_leek'],
     install_requires = ['django>=1.11'],
     include_package_data=True,
