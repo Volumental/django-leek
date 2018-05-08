43daa6b8409bebcf92ba4147f31e7637db63e92b
Samuel Carlsson <samuel.carlsson@volumental.com>
2018-05-08 15:04:21 +1200

    decorator api

--- test_app/app.py
+++ test_app/app.py
@@ -2,15 +2,24 @@ from django.conf.urls import url
 from django.http import HttpResponse
 from django.shortcuts import render
 
-from django_leek.API import push_task_to_queue
+from django_leek.API import Leek, push_task_to_queue
 
-def task():
-    print('Executed on queue!')
+leek = Leek()
+
+@leek.task
+def hello(to):
+    print('Hello {}!'.format(to))
 
 
 def index(request):
     if 'queue' in request.GET:
-        push_task_to_queue(task)
+        # Run sync
+        hello(to='sync')
+        
+        # Run async
+        hello.offload(to='new')
+
+        push_task_to_queue(hello, to='old')
         return render(request, 'index.html', {'message': '✓ task queued'})
 
     return render(request, 'index.html')
