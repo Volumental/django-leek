428feee3167c683acb95fee4e645d06cf47b518e
Samuel Carlsson <samuel.carlsson@volumental.com>
2018-05-16 14:01:44 +1200

    ex: unit test keyboard interrupt

--- django_leek/tests.py
+++ django_leek/tests.py
@@ -4,8 +4,12 @@ import socketserver
 from django.test import TestCase
 from django.core.management import call_command
 
+@patch.object(socketserver.TCPServer, 'serve_forever')
 class LeekCommandTestCase(TestCase):
-    @patch.object(socketserver.TCPServer, 'serve_forever')
     def test_leek(self, serve_forever):
         call_command('leek')
         serve_forever.assert_called()
+
+    def test_keyboard_interrupt(self, serve_forever):
+        serve_forever.side_effect = KeyboardInterrupt
+        call_command('leek')
