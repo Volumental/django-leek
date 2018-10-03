73029abab9400cb05c98a7e960cdbdc07e002db3
Silvia Scalisi <silvia.scalisi@volumental.com>
2018-10-03 10:36:03 +0200

    New version in setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open('README.md') as f:
 
 setup(
     name='django-leek',
-    version='0.9.1',
+    version='0.9.2',
     packages=find_packages(exclude=['test_app']),
     install_requires = ['django>=1.11'],
     include_package_data=True,
