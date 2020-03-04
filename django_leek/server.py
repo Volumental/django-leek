1c70540c02d283bb52bdbc5ab51831d509f4f785
Samuel Carlsson <samuel.carlsson@volumental.com>
2020-03-04 18:37:04 +0100

    refactor: rm dict

--- django_leek/server.py
+++ django_leek/server.py
@@ -9,9 +9,6 @@ import django
 log = logging.getLogger(__name__)
 
 
-Dcommands = {}
-
-
 def target(queue):
     django.setup()
     log.info('Worker Starts')
@@ -53,44 +50,34 @@ class TaskSocketServer(socketserver.BaseRequestHandler):
     def handle(self):
         try:
             data = self.request.recv(5000).strip()
-            if data in Dcommands.keys():
-                log.info('Got command: "{}"'.format(data))
-                try:
-                    worker_response = Dcommands[data]()
-                    response = (True, worker_response.encode(),)
-                    self.request.send(str(response).encode())
-                except Exception as e:
-                    log.exception("command failed")
-                    response =  (False, "TaskServer Command: {}".format(e).encode(),)
-                    self.request.send(str(response).encode())
-            else:
-                # assume a serialized task
-                log.info('Got a task')
-                try:
-                    task_id = int(data.decode())
-                    
-                    # Connection are closed by tasks, force it to reconnect
-                    django.db.connections.close_all()
-                    queued_task = load_task(task_id=task_id)
-                    
-                    # Ensure pool got a worker processing it
-                    pool_name = queued_task.pool or self.DEFAULT_POOL
-                    pool = self.pools.get(pool_name)
-                    if pool is None or not pool.worker.is_alive():
-                        # Spawn new pool
-                        log.info('Spawning new pool: {}'.format(pool_name))
-                        self.pools[pool_name] = Pool()
-                        self.pools[pool_name].worker.start()
 
-                    task = helpers.unpack(queued_task.pickled_task)
-                    self.pools[pool_name].queue.put(task)
+            # assume a serialized task
+            log.info('Got a task')
+            try:
+                task_id = int(data.decode())
+                
+                # Connection are closed by tasks, force it to reconnect
+                django.db.connections.close_all()
+                queued_task = load_task(task_id=task_id)
+                
+                # Ensure pool got a worker processing it
+                pool_name = queued_task.pool or self.DEFAULT_POOL
+                pool = self.pools.get(pool_name)
+                if pool is None or not pool.worker.is_alive():
+                    # Spawn new pool
+                    log.info('Spawning new pool: {}'.format(pool_name))
+                    self.pools[pool_name] = Pool()
+                    self.pools[pool_name].worker.start()
 
-                    response = (True, "sent")
-                    self.request.send(str(response).encode())
-                except Exception as e:
-                    log.exception("failed to queue task")
-                    response = (False, "TaskServer Put: {}".format(e).encode(),)
-                    self.request.send(str(response).encode())
+                task = helpers.unpack(queued_task.pickled_task)
+                self.pools[pool_name].queue.put(task)
+
+                response = (True, "sent")
+                self.request.send(str(response).encode())
+            except Exception as e:
+                log.exception("failed to queue task")
+                response = (False, "TaskServer Put: {}".format(e).encode(),)
+                self.request.send(str(response).encode())
 
         except OSError as e:
             # in case of network error, just log
